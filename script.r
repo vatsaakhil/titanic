7adb399d027246c2f55e32c9cedc83d8ccec036f
Akhil Vatsa <vatsaakhil@gmail.com>
2018-06-16 20:34:50 +0530

    Creation of model using RF and prediction based on it

--- script.r
+++ script.r
@@ -35,6 +35,34 @@ train$Survived<-as.factor(train$Survived)
 
 #Prediction and Modeling---------------------------------------------------------------------------------#
 
+#Survived equation
+survived_eq<-"Survived~Pclass+Sex+Age+Fare+SibSp+Parch+Embarked"
+
+#Survived formula casting
+survival<-as.formula(survived_eq)
+
+#Random Forest package
+install.packages("randomForest")
+library(randomForest)
+
+#Creating the model using RF
+model<-randomForest(formula=survival, data=train, ntree=500,mtry=3,nodesize=0.01*nrow(train)) 
+
+
+features_eq<-"Pclass+Sex+Age+Fare+SibSp+Parch+Embarked"
+
+#Prediction of Survival 
+Survived<-predict(model, newdata=test)
+
+
+#Creating the prediction and new data frame
+PassengerId<-test$PassengerId
+output.df<-as.data.frame(PassengerId)          #New data frame created 
+output.df$Survived<-Survived 
+
+write.csv(output.df, file="output.csv", row.names = FALSE)
+
+
 
 
 
